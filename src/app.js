894eb611004e970fcf340cc0b488350c6b6ddce2
shikher Jaitly <SHIKHER.JAITLY@GMAIL.COM>
2024-02-14 22:22:15 +0530

    created folder structure

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
 //contains the business logic 
+import express  from "express";
+
+const app = express();
+
+app.get('/', (req,res) =>{
+    res.send('welcome to dumbbelldoor');
+});
+
+export { app };
