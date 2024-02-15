befc68c69f3049d7f5430d4b91c8d38b918666be
gurudattpuranik25 <gurudattpuranik35@gmail.com>
2024-02-16 00:12:53 +0530

    completed trainer schema design

--- src/models/trainer.model.js
+++ src/models/trainer.model.js
@@ -6,15 +6,20 @@ const trainerSchema = new mongoose.Schema(
     name: { type: String },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    profilePicture: { type: String },
+    description: { type: String },
+    yearsOfExperience: { type: String },
     certifications: [{ type: String }],
-    specialties: [{ type: String }],
+    specializations: [{ type: String }],
+    typesOfServices: [{ type: String }],
     availability: {
       days: [{ type: String }],
       startTime: { type: String },
       endTime: { type: String },
     },
-    rating: { type: Number, default: 0 },
-    reviews: [
+    location: { type: String },
+    phoneNumber: { type: String },
+    testimonials: [
       {
         customerId: { type: mongoose.Schema.Types.ObjectId, ref: "Customer" },
         rating: { type: Number },
