898d756139a231d754efcd5caa450ad880a9da10
shikher Jaitly <SHIKHER.JAITLY@GMAIL.COM>
2024-02-27 22:07:17 +0530

    resturn statement update

--- src/controllers/authentication.controller.js
+++ src/controllers/authentication.controller.js
@@ -95,7 +95,7 @@ const login = async (req, res) => {
         return res.status(200).json({
           success: true,
           message: "Login successful!",
-          accessToken: token,
+          responseData: { accessToken: token, userDetails: user }
         });
       }
     );
