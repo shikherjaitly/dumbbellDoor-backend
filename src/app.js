12abecd074523558a703fdf6158ffff24d29e8f6
shikher Jaitly <SHIKHER.JAITLY@GMAIL.COM>
2024-03-07 00:44:34 +0530

    added customer profile api

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import errorHandler from "./utils/errorHandler.js";
 import userRouter from "../src/routes/user.route.js";
 import trainerRouter from "../src/routes/trainer.route.js";
 import bookingRouter from "../src/routes/booking.route.js";
+import customerRouter from "../src/routes/customer.route.js";
 
 const app = express();
 dotenv.config();
@@ -66,6 +67,7 @@ app.use(cors());
 app.use("/api/auth", userRouter);
 app.use("/api/trainer", trainerRouter);
 app.use("/api/bookings", bookingRouter);
+app.use("/api/customer", customerRouter);
 
 app.use("*", (req, res) => {
   errorHandler(res, 404, "Oops...something went wrong!");
